2d59b4e60204234082a282ef4163ab2620584ec1
saikrishna2409 <34795704+saikrishna2409@users.noreply.github.com>
2020-03-09 23:45:46 -0700

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -2,8 +2,8 @@ public class Sum
 {
 public static void main(String[] args)
 {
-int a =args[0];
-int b=args[1];
+int a =integer.parseInt(args[0]);
+int b=integer.parseInt(args[1]);
 int sum= a+b;
 System.out.println(sum);
 }
